21238a1d8b3071dd9b347a6b6d78339a6cae31fc
ddialar <ddialar@gmail.com>
2021-07-05 00:21:35 +0100

    refactor: Remove the API Error mapper because it won't be used any more.

--- src/infrastructure/mappers/index.ts
+++ src/infrastructure/mappers/index.ts
@@ -1,3 +1,2 @@
-export * from './mapErrorFromApiToDomain.mapper'
 export * from './mapNluAFromApiToDomain.mapper'
 export * from './mapNluBFromApiToDomain.mapper'
--- src/infrastructure/mappers/mapErrorFromApiToDomain.mapper.ts
+++
@@ -1,6 +0,0 @@
-import { ApiError } from '@types'
-
-export const mapErrorFromApiToDomain = (data: any): ApiError => ({
-  error: true,
-  message: data?.message || 'There was an error with your request.'
-})
