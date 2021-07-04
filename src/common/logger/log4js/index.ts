39461c1fd0ccc5ee9f9991ad7483e0a9aeb3897e
ddialar <ddialar@gmail.com>
2021-07-05 00:20:28 +0100

    chore: Define the 'dataSourceLogger'.

--- src/common/logger/log4js/index.ts
+++ src/common/logger/log4js/index.ts
@@ -5,3 +5,4 @@ type LogTypes = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal' | 'mark'
 export const appLogger = (logType: LogTypes, message: string) => logger[logType](`[app] - ${message}`)
 export const serverLogger = (logType: LogTypes, message: string) => logger[logType](`[server] - ${message}`)
 export const nluLogger = (logType: LogTypes, message: string) => logger[logType](`[nlu] - ${message}`)
+export const dataSourceLogger = (logType: LogTypes, message: string) => logger[logType](`[dataSource] - ${message}`)
