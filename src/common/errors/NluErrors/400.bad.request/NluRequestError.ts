f332b0bb6538df0fb4ee24cfa011953e4fe5c67f
ddialar <ddialar@gmail.com>
2021-07-05 23:33:29 +0100

    refactor: Remove the 'NluRequestError' class.

--- src/common/errors/NluErrors/400.bad.request/NluRequestError.ts
+++
@@ -1,9 +0,0 @@
-import { ApiError, BAD_REQUEST } from '@errors'
-
-const message = 'Wrong NLU request parameters error.'
-
-export class NluRequestError extends ApiError {
-  constructor (description?: string) {
-    super(BAD_REQUEST, message, description)
-  }
-}
--- src/common/errors/NluErrors/400.bad.request/index.ts
+++ src/common/errors/NluErrors/400.bad.request/index.ts
@@ -1,2 +1,1 @@
-export * from './NluRequestError'
 export * from './NluModelNotValidError'
